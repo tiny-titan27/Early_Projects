ee5c9d21f09168e8402bdc45347870c8325f0c81
jacob-katsifarakis <149403151+tiny-titan27@users.noreply.github.com>
2024-06-10 17:20:11 -0500

    Update Fibbonacci Sequence.py

--- Fibbonacci Sequence.py
+++ Fibbonacci Sequence.py
@@ -12,11 +12,11 @@ def fibonacci_sequence(n):
         count += 1
     # edgecases
     if n == 1:
-        print(n1)
+        return(n1)
     elif n == 2:
-        print(n2)
+        return(n2)
     else:
-        print(n3)
+        return(n3)
 
 
 fibonacci_sequence(101)
