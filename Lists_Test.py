61d6d8ee8dbd40fa5f9a9548cbfe4c6230e58bc3
jacob-katsifarakis <149403151+tiny-titan27@users.noreply.github.com>
2024-06-30 22:44:15 -0500

    Add files via upload

---
+++ Lists_Test.py
@@ -0,0 +1,16 @@
+# Testing using lists and making functions that reference specific portions of a list in a list
+segment_1 = [1, 2, 0]
+segment_2 = [3, 0, 0]
+segment_3 = [-2, 1, 0]
+
+
+def list_maker(n):
+    count = 1
+    mylist = []
+    while count < n+1:
+        mylist.append(WIP)
+        count = count + 1
+    print(mylist)
+
+
+list_maker(3)
